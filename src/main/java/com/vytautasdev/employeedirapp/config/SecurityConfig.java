3d2cc8f95a7a606802f4d6531e48c4a50ee5a667
vytautasdev <vytautasdev@gmail.com>
2022-07-12 20:52:04 +0100

    First Commit

--- src/main/java/com/vytautasdev/employeedirapp/config/SecurityConfig.java
+++ src/main/java/com/vytautasdev/employeedirapp/config/SecurityConfig.java
@@ -39,7 +39,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/resources/**").permitAll()
                 .and()
                 .formLogin()
-                .loginPage("/showMyLoginPage")
+                .loginPage("/signIn")
                 .loginProcessingUrl("/authenticateTheUser")
                 .permitAll()
                 .and()
--- src/main/java/com/vytautasdev/employeedirapp/controller/LoginController.java
+++ src/main/java/com/vytautasdev/employeedirapp/controller/LoginController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class LoginController {
 
-    @GetMapping("/showMyLoginPage")
+    @GetMapping("/signIn")
     public String showLoginPage() {
 
         return "fancy-login";
